2d1eb3de94c7e8d96f55153e721081a7fde6658d
Yadori_Ouchi <yadoriouchi@gmail.com>
2024-05-10 12:59:17 +0900

    障害物の素追加

--- ClickRipple.cpp
+++ ClickRipple.cpp
@@ -1,7 +1,10 @@
 #include "DxLib.h"
+#include <stdlib.h>
+#include <time.h>
 #include <math.h>
 #include <vector>
 #include <string>
+#include "data.h"
 
 using std::vector;
 using std::string;
@@ -9,12 +12,14 @@ using std::string;
 #define SCRHEIGHT 640
 #define SCRWIDTH 960
 
+#define OBSPOSMAX 16
+
 #define RADMAX 100
 #define BALLRAD 5
 
 #define PI 3.141592 
 
-//#define DEBUG 0
+#define DEBUG 1
 
 enum eFlags {
 	NONE = 0,
@@ -38,41 +43,32 @@ typedef struct sDblAxis {
 	double Y;
 } sDblAxis;
 
-const char easy[161] = {
-	"01111110000110000001111001100112"
-	"01100000011001100110000001100112"
-	"01111110011001100001111000011002"
-	"01100000011111100000011000011002"
-	"01111110011001100111100000011002"
-};
+const int white = GetColor(255, 255, 255);
 
-const char normal[241] = {
-	"011001100001100001111000011001100001100001100002"
-	"011101100110011001100110011111100110011001100002"
-	"011111100110011001111000011001100110011001100002"
-	"011011100110011001100110011001100111111001100002"
-	"011001100001100001100110011001100110011001111112"
+class cObstacle {
+    private:
+		sAxis obsPos = { 0, 0 };
+		int obsRad = 0;
+    public:
+		cObstacle(int index = 8) {
+			index = index % OBSPOSMAX;
+			obsRad = rand() % RADMAX;
+			obsPos = { SCRWIDTH + 100, obsPosData[index] };
+		};
+		void drawObstacle(void) {
+			DrawCircle(obsPos.X, obsPos.Y, obsRad, white, TRUE);
+			obsPos.X--;
+		}
 };
-
-const char hard[161] = {
-	"01100110000110000111100001111002"
-	"01100110011001100110011001100112"
-	"01111110011001100111100001100112"
-	"01100110011111100110011001100112"
-	"01100110011001100110011001111002"
-};
-
-int white = GetColor(255, 255, 255);
-sAxis sInput = { 0, 0 };
 
 double RadToDig(double);
 sAxis GetInput(void);
-void DrawAA(int, int, char[]);
 
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
 	if (DxLib_Init() == -1){
 		return -1;
 	}
+	srand((unsigned)time(NULL));
 
 	int rad = 0, radMod = rad, radSpeed = 2;
 	int ballColor = 255;
@@ -88,6 +84,7 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	double accRate = 8.0f;
 	sDblAxis sAcc = { 0.0f, -3.0f };
 	double gravity = 0.15;
+	sAxis sInput = { 0, 0 };
 
 	SetWindowText("ClickRipple");
 	SetGraphMode(SCRWIDTH, SCRHEIGHT, 32);
@@ -112,104 +109,107 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	sVel.X *= -1;
 	}
 
-	}*/
-	while (!CheckHitKey(KEY_INPUT_ESCAPE)) {
-		switch (state) {
-			case(TITLE):
-				ClearDrawScreen();
-				timer++;
-				SetFontSize(80);
-				DrawString(230, SCRHEIGHT / 3, "CLICK RIPPLE", white);//12字
-				if (timer < 30) {
-					SetFontSize(30);
-					DrawString(320, (SCRHEIGHT / 3) * 2, "Press SPACE to Start", white);//20字
-					//DrawFormatString(0, 0, white, "%d", timer);
-				}
-				else if (timer > 60) {
-					timer = 0;
-				}
+	}*/ //ボールの演算のサンプル
 
-				if (CheckHitKey(KEY_INPUT_SPACE)) {
-					state = MENU;
+	//while (!CheckHitKey(KEY_INPUT_ESCAPE)) {
+	//	switch (state) {
+	//		case(TITLE):
+	//			ClearDrawScreen();
+	//			timer++;
+	//			SetFontSize(80);
+	//			DrawString(230, SCRHEIGHT / 3, "CLICK RIPPLE", white);//12字
+	//			if (timer < 30) {
+	//				SetFontSize(30);
+	//				DrawString(320, (SCRHEIGHT / 3) * 2, "Press SPACE to Start", white);//20字
+	//				//DrawFormatString(0, 0, white, "%d", timer);
+	//			}
+	//			else if (timer > 60) {
+	//				timer = 0;
+	//			}
+
+	//			if (CheckHitKey(KEY_INPUT_SPACE)) {
+	//				state = MENU;
+	//			}
+	//			break;
+	//		case(MENU):
+	//			ClearDrawScreen();
+	//			//sInput = GetInput();
+	//			//DrawFormatString(0, 0, white, "x: %d, y: %d", sInput.X, sInput.Y);
+	//			break;
+	//		case(PLAY):
+	//			break;
+	//		case(OVER):
+	//			break;
+	//	}
+	//	ScreenFlip();
+	//}
+
+	cObstacle test;
+
+	while (!CheckHitKey(KEY_INPUT_ESCAPE)) {
+		timer += 1;
+		ClearDrawScreen();
+		DrawCircle(sBall.X, sBall.Y, BALLRAD, white); //ボール
+		GetMousePoint(&sMouse.X, &sMouse.Y);
+		dist = sqrtf((sBall.X - sMouse.X) * (sBall.X - sMouse.X) + (sBall.Y - sMouse.Y) * (sBall.Y - sMouse.Y));
+		ballATT = atan2(sMouse.Y - sBall.Y, sMouse.X - sBall.X);
+		ballCos = cos(ballATT);
+		ballSin = sin(ballATT);
+		if (GetMouseInput() & MOUSE_INPUT_LEFT && !(flag & COLLISION)) {
+			timer = 1;
+			rad = (rad + radSpeed) % RADMAX;
+			DrawCircle(sMouse.X, sMouse.Y, rad, white, FALSE);
+			if (BALLRAD + rad > dist) {
+				flag = COLLISION;
+				if (ballCos < 0 && 0 < ballSin ) { //ボールがマウスの右方向
+					sAcc.X = -((ballCos * rad) / accRate);
+					sAcc.Y = -((ballSin * rad) / accRate);
 				}
-				break;
-			case(MENU):
-				ClearDrawScreen();
-				//sInput = GetInput();
-				//DrawFormatString(0, 0, white, "x: %d, y: %d", sInput.X, sInput.Y);
-				DrawAA(0, 0, &normal);
-				break;
-			case(PLAY):
-				break;
-			case(OVER):
-				break;
+				else if(0 < ballSin){
+					sAcc.X = -((ballCos * rad) / accRate);
+					sAcc.Y = -((ballSin * rad) / accRate);
+				}
+			}
 		}
+		else {
+			rad = 0;
+		}
+
+		sAcc.Y += gravity;
+		sBall.X += sAcc.X;
+		sBall.Y += sAcc.Y;
+
+		if (sBall.Y > SCRHEIGHT) {
+			sAcc = { 0, 0 };
+			DrawFormatString(SCRWIDTH / 3, SCRHEIGHT / 2, white, "GAME OVER!");
+		}
+		if (sBall.Y < 0) {
+			sAcc.Y *= -1;
+		}
+		if (sBall.X < 0 || SCRWIDTH < sBall.X) {
+			sAcc.X *= -1;
+		}
+
+		DrawLine(sBall.X, sBall.Y, sMouse.X, sMouse.Y, white); //アシスト表示
+#ifdef DEBUG
+		DrawFormatString(0, 0, white, "atan2の値: %f, 角度に直した値: %f", ballATT, RadToDig(ballATT));
+		DrawFormatString(0, 20, white, "ボールのcos(x座標): %f\nボールのsin(y座標)：%f", ballCos, ballSin);
+		DrawFormatString(0, 60, white, "%ボールとマウスの距離: %4f\nボールとマウスの半径の合計: %d リップルの半径: %d", dist, (BALLRAD + rad), rad);
+		DrawFormatString(0, 100, white, "マウス x: %d, y: %d, ボール x: %2f, y: %2f", sMouse.X, sMouse.Y, sBall.X, sBall.Y);
+		DrawFormatString(0, 120, white, "タイマー: %d, X軸加速度: %2f, Y軸加速度: %2f", timer, sAcc.X, sAcc.Y);
+
+		if (CheckHitKey(KEY_INPUT_SPACE)) {
+			sAcc.X = 0.0f;
+			sAcc.Y = -5.0f;
+		}
+#endif
+		if ((timer % timeWait) == 0) {
+			flag = NONE;
+		}
+
+		test.drawObstacle();
 		ScreenFlip();
 	}
-
-//	while (!CheckHitKey(KEY_INPUT_ESCAPE)) {
-//		timer += 1;
-//		ClearDrawScreen();
-//		DrawCircle(sBall.X, sBall.Y, BALLRAD, white); //ボール
-//		GetMousePoint(&sMouse.X, &sMouse.Y);
-//		dist = sqrtf((sBall.X - sMouse.X) * (sBall.X - sMouse.X) + (sBall.Y - sMouse.Y) * (sBall.Y - sMouse.Y));
-//		ballATT = atan2(sMouse.Y - sBall.Y, sMouse.X - sBall.X);
-//		ballCos = cos(ballATT);
-//		ballSin = sin(ballATT);
-//		if (GetMouseInput() & MOUSE_INPUT_LEFT && !(flag & COLLISION)) {
-//			timer = 1;
-//			rad += radSpeed;
-//			radMod = rad % RADMAX;
-//			DrawCircle(sMouse.X, sMouse.Y, radMod, white, FALSE);
-//			if (BALLRAD + radMod > dist) {
-//				flag = COLLISION;
-//				if (ballCos < 0 && 0 < ballSin ) { //ボールがマウスの右方向
-//					sAcc.X = -((ballCos * radMod) / accRate);
-//					sAcc.Y = -((ballSin * radMod) / accRate);
-//				}
-//				else if(0 < ballSin){
-//					sAcc.X = -((ballCos * radMod) / accRate);
-//					sAcc.Y = -((ballSin * radMod) / accRate);
-//				}
-//			}
-//		}
-//		else {
-//			rad = radMod = 0;
-//		}
-//
-//		sAcc.Y += gravity;
-//		sBall.X += sAcc.X;
-//		sBall.Y += sAcc.Y;
-//
-//		if (sBall.Y > SCRHEIGHT) {
-//			sAcc = { 0, 0 };
-//			DrawFormatString(SCRWIDTH / 3, SCRHEIGHT / 2, white, "GAME OVER!");
-//		}
-//		if (sBall.Y < 0) {
-//			sAcc.Y *= -1;
-//		}
-//		if (sBall.X < 0 || SCRWIDTH < sBall.X) {
-//			sAcc.X *= -1;
-//		}
-//
-//		DrawLine(sBall.X, sBall.Y, sMouse.X, sMouse.Y, white); //アシスト表示
-//#ifdef DEBUG
-//		DrawFormatString(0, 0, white, "atan2の値: %f, 角度に直した値: %f", ballATT, RadToDig(ballATT));
-//		DrawFormatString(0, 20, white, "ボールのcos(x座標): %f\nボールのsin(y座標)：%f", ballCos, ballSin);
-//		DrawFormatString(0, 60, white, "%ボールとマウスの距離: %4f\nボールとマウスの半径の合計: %d リップルの半径: %d", dist, (BALLRAD + radMod), radMod);
-//		DrawFormatString(0, 100, white, "マウス x: %d, y: %d, ボール x: %2f, y: %2f", sMouse.X, sMouse.Y, sBall.X, sBall.Y);
-//		DrawFormatString(0, 120, white, "タイマー: %d, X軸加速度: %2f, Y軸加速度: %2f", timer, sAcc.X, sAcc.Y);
-//
-//		if (CheckHitKey(KEY_INPUT_SPACE)) {
-//			sAcc.X = 0.0f;
-//			sAcc.Y = -5.0f;
-//		}
-//#endif
-//		if ((timer % timeWait) == 0) {
-//			flag = NONE;
-//		}
-//		ScreenFlip();
-//	}
 
 	DxLib_End();
 	return 0;
@@ -235,23 +235,4 @@ sAxis GetInput(void) {
 	}
 
 	return { 0, 0 };
-}
-
-void DrawAA(int x, int y, const char AA[]) {
-	int xOri = x, yOri = y;
-	for (int i = 0; i < 241; i++) {
-		switch (AA[i]) {
-			case('0'):
-				x += 5;
-				break;
-			case('1'):
-				DrawBox(x, y, x + 5, y + 10, white, 1);
-				x += 5;
-				break;
-			case('2'):
-				x = xOri;
-				y += 10;
-				break;
-		}
-	}
 }
